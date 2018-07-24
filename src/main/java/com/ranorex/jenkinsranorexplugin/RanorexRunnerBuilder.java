d67c22850e98be52b7efacdb7d39fec2bdbf4244
Markus Stögerer <markus.stoegerer@gmail.com>
2018-07-24 12:05:24 +0200

    Refactor Reporting

--- src/main/java/com/ranorex/jenkinsranorexplugin/RanorexRunnerBuilder.java
+++ src/main/java/com/ranorex/jenkinsranorexplugin/RanorexRunnerBuilder.java
@@ -215,7 +215,7 @@ public class RanorexRunnerBuilder extends Builder {
             // ReportFilename
             if (! StringUtil.isNullOrSpace(rxReportFile)) {
                 if (! FileUtil.isAbsolutePath(rxReportFile)) {
-                    usedRxReportFile = rxReportFile;
+                    usedRxReportFile = FileUtil.removeFileExtension(rxReportFile);
                 } else {
                     LOGGER.println("'" + rxReportFile + "' is not a valid Ranorex Report filename");
                     return false;
@@ -223,7 +223,6 @@ public class RanorexRunnerBuilder extends Builder {
             } else {
                 usedRxReportFile = "%S_%Y%M%D_%T";
             }
-            usedRxReportFile = FileUtil.removeFileExtension(usedRxReportFile);
             jArguments.add("/reportfile:" + usedRxReportDirectory + usedRxReportFile + "." + rxReportExtension);
 
             // JUnit compatible Report
@@ -245,7 +244,7 @@ public class RanorexRunnerBuilder extends Builder {
                 // Zipped Report File Name
                 if (! StringUtil.isNullOrSpace(rxZippedReportFile)) {
                     if (! FileUtil.isAbsolutePath(rxZippedReportFile)) {
-                        usedRxZippedReportFile = rxZippedReportFile;
+                        usedRxZippedReportFile = FileUtil.removeFileExtension(rxZippedReportFile);
                     } else {
                         LOGGER.println("'" + rxZippedReportFile + "' is not a valid Ranorex Report filename");
                         return false;
@@ -253,7 +252,7 @@ public class RanorexRunnerBuilder extends Builder {
                 } else {
                     usedRxZippedReportFile = usedRxReportFile;
                 }
-                usedRxZippedReportFile = FileUtil.removeFileExtension(usedRxZippedReportFile);
+
                 jArguments.add("/zipreportfile:" + usedRxZippedReportDirectory + usedRxZippedReportFile + ZIPPED_REPORT_EXTENSION);
             }
 
